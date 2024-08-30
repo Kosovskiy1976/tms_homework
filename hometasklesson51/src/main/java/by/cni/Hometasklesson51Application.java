52c6c51301b5e24cdcdf34052885ab61357cc227
Andrew Kosovskiy <andreykosovskiy1976@gmail.com>
2024-08-30 23:09:14 +0300

    HomeTaskLesson51

---
+++ hometasklesson51/src/main/java/by/cni/Hometasklesson51Application.java
@@ -0,0 +1,13 @@
+package by.cni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hometasklesson51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hometasklesson51Application.class, args);
+    }
+
+}
